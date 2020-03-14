35ca4e3614f10fda0d33305802ac32e60b515433
richjmaynard <rich@maynard.org.uk>
2020-03-14 17:24:09 +0000

    Add files via upload

---
+++ General/converter.py
@@ -0,0 +1,18 @@
+file_name=input("Filename (no extension):")
+print("Converting ",file_name,".pbm to ",file_name,".bin")
+
+file=open(file_name+".pbm","rb")
+file.readline()
+file.readline()
+file.readline()
+data = bytearray(file.read())
+file.close()
+
+
+file2=open(file_name+".bin","wb")
+file2.write(data)
+file2.close()
+print("Done")
+
+
+
