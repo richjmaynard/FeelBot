87553ea272c73f8b54cc4a3ad8363518c73e1b4a
richjmaynard <rich@maynard.org.uk>
2020-03-16 16:53:19 +0000

    Add files via upload

---
+++ D1 Mini Servo/secrets.py
@@ -0,0 +1,10 @@
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'wifi_ssid',
+    'password' : 'wifi_password'
+}
+
+
